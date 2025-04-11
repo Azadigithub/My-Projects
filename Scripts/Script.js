94e9c6aab15974dffbd6cec020c73ab7f44c8f81
Ali <aliazadigithub@gmail.com>
2025-04-11 12:32:27 -0700

    Header design

---
+++ Scripts/Script.js
@@ -0,0 +1,9 @@
+let searchinput = document.getElementById('search')
+let searchicon = document.getElementById('search-icon')
+
+
+searchicon.addEventListener('click',function(){
+    console.log('lll');
+    searchinput.classList.toggle('active')
+    
+})
